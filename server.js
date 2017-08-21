0a0df17c8163f11d96b4e55f522f47b1e89fbd12
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-08-21 20:54:14 +0300

    test

--- server.js
+++ server.js
@@ -93,7 +93,35 @@ app.get('/pagecount', function (req, res) {
 
 app.get('/sendRequest', function(req, res){
   res.send("success");
+  let address = "https://fcm.googleapis.com/fcm/send"
+  post(address, {to:'aUniqueKey', data:{hello:"Message"}});
+
 });
+
+
+function post(path, params, method) {
+  method = method || "post"; // Set method to post by default if not specified.
+
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
+
+  for(var key in params) {
+      if(params.hasOwnProperty(key)) {
+          var hiddenField = document.createElement("input");
+          hiddenField.setAttribute("type", "hidden");
+          hiddenField.setAttribute("name", key);
+          hiddenField.setAttribute("value", params[key]);
+
+          form.appendChild(hiddenField);
+       }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
 
 // error handling
 app.use(function(err, req, res, next){
