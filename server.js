0a5f3cfc85b458bed467c74740fbd33ca393d766
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-08-21 20:28:44 +0300

    test

--- server.js
+++ server.js
@@ -91,6 +91,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/sendRequest', function(req, res){
+  res.send("success")
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
